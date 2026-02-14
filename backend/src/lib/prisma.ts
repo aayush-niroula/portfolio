261b0ade7b854066ccc9ef2d225d5e1c8e6a680c
aayush-niroula <ayushniroula644@gmail.com>
2026-02-14 16:53:37 +0545

    prisma

--- backend/src/lib/prisma.ts
+++ backend/src/lib/prisma.ts
@@ -1,11 +1,12 @@
 import { PrismaClient } from '@prisma/client';
+import { PrismaPg } from '@prisma/adapter-pg';
+import { Pool } from 'pg';
 
-const prisma = new PrismaClient({
-  datasources: {
-    db: {
-      url: process.env.DATABASE_URL,
-    },
-  },
-});
+const connectionString = process.env.DATABASE_URL;
+
+const pool = new Pool({ connectionString });
+const adapter = new PrismaPg(pool);
+
+const prisma = new PrismaClient({ adapter });
 
 export default prisma;
